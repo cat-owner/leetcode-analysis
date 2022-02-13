0b57478e5ecf62295f4753ad88cc60fb511b300c
wangyuancun <wangyc5@yonghui.cn>
2022-02-13 21:28:34 +0800

    初始化分支

---
+++ src/main/java/com/wyc/dream/leetcodeanalysis/LeetcodeAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.wyc.dream.leetcodeanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeAnalysisApplication.class, args);
+	}
+
+}
