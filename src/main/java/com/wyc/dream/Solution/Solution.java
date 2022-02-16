c7450f0b4e0fd029e8d1e01dae5ea315cb464297
wangyuancun <wangyc5@yonghui.cn>
2022-02-16 10:26:09 +0800

    两数之和

---
+++ src/main/java/com/wyc/dream/Solution/Solution.java
@@ -0,0 +1,10 @@
+package com.wyc.dream.twoSum;
+
+/**
+ * @author: wangyuancun
+ * @description: 两数之和;
+ * @date: 2022/2/13
+ */
+public class TwoSum {
+    
+}
--- src/main/java/com/wyc/dream/twoSum/TwoSum.java
+++
@@ -1,8 +0,0 @@
-package com.wyc.dream.twoSum;
-/**
-  * @author:         wangyuancun
-  * @description:     java类作用描述 
-  * @date:     2022/2/13
- */
-public class TwoSum {
-}
